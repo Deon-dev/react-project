b5b73adf94eeaf075e761169655b0f5c57802714
Deon-dev <kwabenagideonagu@gmail.com>
2025-09-29 02:44:31 +0100

    applied props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 
 import './App.css'
 
-function App() {
-  const subject = "Deonthedeveloper";
+function App(props) {
+  // const subject = "Deonthedeveloper";
 
   return (
     <>
       <h1>Welcome to my Trial run React App</h1>
       <button type="button" className='primary'>Click on me</button>
 
-      <p>Hello, {subject}</p>
-      <p>Hello, {subject.toUpperCase()}</p>
+      <p>Hello, {props.subject}</p>
+      {/* <p>Hello, {subject.toUpperCase()}</p> */}
     </>
   )
 }
