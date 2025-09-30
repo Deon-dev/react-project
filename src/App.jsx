b15a62a60f8b5058877544fb5798819eb93176c4
Deon-dev <kwabenagideonagu@gmail.com>
2025-09-30 04:31:15 +0100

    added style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,82 @@
+import './index.css'
 
-import './App.css'
 
-function App(props) {
-  // const subject = "Deonthedeveloper";
-
+function App() {
   return (
-    <>
-      <h1>Welcome to my Trial run React App</h1>
-      <button type="button" className='primary'>Click on me</button>
+    <div className="todoapp stack-large">
+      <h1>DeonMatic</h1>
+      {/* <h1 hidden={true}>Who are You??</h1> */}
+      <form action="">
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">What needs to be done?</label>
+        </h2>
 
-      <p>Hello, {props.subject}</p>
-      {/* <p>Hello, {subject.toUpperCase()}</p> */}
-    </>
+        <input 
+          type="text" 
+          name="text" 
+          id="new-todo-input" 
+          autoComplete="off" 
+        />
+
+        <button type="submit" className="btn btn__primary btn__lg">Add</button>
+      </form>
+      
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul 
+        role="list" 
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-0" type="checkbox" defaultChecked />
+            <label htmlFor="todo-0" className="todo-label">Eat</label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-1" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-1">
+              Sleep
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Sleep</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Sleep</span>
+            </button>
+          </div>
+        </li>
+        </ul>
+    </div>
   )
 }
 
