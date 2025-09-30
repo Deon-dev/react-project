86104ebdefa5832a63b6395259d93ff0dfc315f7
Deon-dev <kwabenagideonagu@gmail.com>
2025-09-30 05:12:02 +0100

    Brokedown the app into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './index.css'
+import Todo from './components/Todo'
 
 
-function App() {
+function App(props) {
+  const taskList = props.tasks?.map((task) => (
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>
+  ));
+
   return (
     <div className="todoapp stack-large">
       <h1>DeonMatic</h1>
@@ -44,40 +49,10 @@ function App() {
         role="list" 
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-0" type="checkbox" defaultChecked />
-            <label htmlFor="todo-0" className="todo-label">Eat</label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Eat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Eat</span>
-            </button>
-          </div>
-        </li>
-
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              Sleep
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Sleep</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Sleep</span>
-            </button>
-          </div>
-        </li>
-        </ul>
+        {taskList}
+      </ul>
     </div>
   )
 }
 
-export default App
+export default App;
